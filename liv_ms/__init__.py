c2517acbd293d7d7b6560dd38fe6fae4a0a72853
neilswainston <neilswainston@neils-mbp.home>
2019-10-17 13:56:33 +0100

    Fixed bugs to due score ordering.

--- liv_ms/__init__.py
+++ liv_ms/__init__.py
@@ -27,11 +27,51 @@ def get_df(chem, spec):
     return chem_df.join(spec_df)
 
 
+def _search(matcher, query_spec, df, num_hits):
+    '''Search.'''
+    import time
+    start = time.time()
+
+    # Search:
+    res = matcher.search(query_spec)
+
+    # Get indexes of top n hits:
+    fnc = partial(_get_top_idxs, n=num_hits)
+    top_idxs = np.apply_along_axis(fnc, 1, -res)
+
+    # Get score data corresponding to top n hits:
+    offset = np.arange(0, res.size, res.shape[1])
+    score_data = np.take(res, offset[:, np.newaxis] + top_idxs)
+
+    # Get match data corresponding to top n hits:
+    fnc = partial(_get_data, data=df[['name', 'smiles']])
+    match_data = np.apply_along_axis(fnc, 1, top_idxs)
+
+    print(time.time() - start)
+
+    return score_data, match_data
+
+
+def _get_top_idxs(arr, n):
+    '''Get sorted list of top indices.'''
+    idxs = np.argpartition(arr, n - 1)[:n]
+
+    # Extra code if you need the indices in order:
+    min_elements = arr[idxs]
+    min_elements_order = np.argsort(min_elements)
+    return idxs[min_elements_order]
+
+
+def _get_data(idxs, data):
+    '''Get data for best matches.'''
+    return data.loc[idxs]
+
+
 def main(args):
     '''main method.'''
-    num_spectra = 10000
-    num_queries = 100
-    num_hits = 5
+    num_spectra = 6000
+    num_queries = 16
+    num_hits = 10
 
     chem, spec = mona.get_spectra(args[0], num_spectra)  # int(args[1]))
     df = get_df(chem, spec)
@@ -41,29 +81,13 @@ def main(args):
     matcher = similarity.SpectraMatcher(spectra)
 
     query_df = df.sample(num_queries)
-    print(query_df['name'])
     query_spec = query_df[['m/z', 'I']].values
 
-    import time
-    start = time.time()
+    score_data, match_data = _search(matcher, query_spec, df, num_hits)
 
-    res = matcher.search(query_spec)
-
-    score_data = -np.partition(-res, 1)
-
-    fnc = partial(_get_data, n=num_hits,
-                  data=df[['name', 'formula', 'smiles']])
-    match_data = np.apply_along_axis(fnc, 1, np.argpartition(-res, 1))
-
-    print(time.time() - start)
+    print(query_df['name'])
     print(score_data)
     print(match_data)
-
-
-def _get_data(row, n, data):
-    '''Get data for best matches.'''
-    idxs = row[:n]
-    return data.loc[idxs]
 
 
 if __name__ == '__main__':
