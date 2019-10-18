a0f85f235787fae1a2d683c23db7321404ddb9a3
neilswainston <neilswainston@neils-mbp.home>
2019-10-18 08:43:25 +0100

    Moved similarity scoring to KDtrees.

--- liv_ms/similarity.py
+++ liv_ms/similarity.py
@@ -6,45 +6,31 @@ All rights reserved.
 @author: neilswainston
 '''
 # pylint: disable=too-few-public-methods
-from collections import defaultdict
-
-from scipy.sparse import coo_matrix
-from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial import KDTree
+import numpy as np
 
 
 class SpectraMatcher():
     '''Class to match spectra.'''
 
-    def __init__(self, spectra, bin_size=0.1, min_val=0, max_val=1000):
-        self.__bin_size = bin_size
+    def __init__(self, spectra, min_val=0, max_val=1000):
         self.__min_val = min_val
         self.__max_val = max_val
-        self.__num_bins = int((max_val - min_val) / self.__bin_size)
+        self.__spec_trees = _get_spec_trees(spectra)
 
-        self.__spec_matrix = self.__bin_spec(spectra)
-
-    def search(self, query):
+    def search(self, queries):
         '''Search.'''
-        query_matrix = self.__bin_spec(query)
-        return cosine_similarity(query_matrix, self.__spec_matrix)
+        res = np.array([_get_similarity_scores(spec_tree, queries)
+                        for spec_tree in self.__spec_trees]).T
 
-    def __bin_spec(self, specs):
-        '''Bin spectrum.'''
-        row = []
-        col = []
-        data = []
+        return res
 
-        for spec_idx, spec in enumerate(specs):
-            binned_spec = defaultdict(int)
 
-            for mass, intensity in zip(*spec):
-                if self.__min_val < mass < self.__max_val:
-                    binned_mass = int(mass / self.__bin_size)
-                    binned_spec[binned_mass] += intensity
+def _get_spec_trees(spectra):
+    '''Get KDTree for each spectrum in spectra.'''
+    return [KDTree(spec) for spec in spectra]
 
-            row.extend([spec_idx] * len(binned_spec))
-            col.extend(binned_spec.keys())
-            data.extend(binned_spec.values())
 
-        return coo_matrix((data, (row, col)),
-                          shape=(len(specs), self.__num_bins))
+def _get_similarity_scores(spec_tree, queries):
+    '''Get similarity score.'''
+    return np.mean(spec_tree.query(queries)[0], axis=1)
--- liv_ms/test/test_similarity.py
+++ liv_ms/test/test_similarity.py
@@ -7,7 +7,6 @@ All rights reserved.
 '''
 import random
 import unittest
-
 from liv_ms import similarity
 
 
@@ -16,27 +15,32 @@ class TestSimilarity(unittest.TestCase):
 
     def test_search(self):
         '''Test search method of similarity module.'''
-        spectra = [[[random.random() * 10 for _ in range(16)],
-                    [random.random() for _ in range(16)]]
+        spectra = [[[random.random() * 100, random.random()]
+                    for _ in range(16)]
                    for _ in range(256)]
 
         matcher = similarity.SpectraMatcher(spectra)
 
-        query = [[0.0, 1.2527, 9.765, 9.78], [0.25, 0.5, 0.75, 0.05]]
-        result = matcher.search([query])
+        queries = [[[random.random() * 100, random.random()]
+                    for _ in range(16)]
+                   for _ in range(32)]
 
-        self.assertEqual(result.shape, (1, 256))
+        result = matcher.search(queries)
+
+        self.assertEqual(result.shape, (32, 256))
 
     def test_search_specific(self):
         '''Test search method of similarity module.'''
-        spectra = [[[1, 2, 5], [1, 1, 1]]]
+        spectra = [[[1, 0.5], [10, 0.5]],
+                   [[1, 0.5], [10, 0.5]],
+                   [[1, 0.5], [10, 0.5]]]
 
         matcher = similarity.SpectraMatcher(spectra)
 
-        query = [[1, 20, 3], [1e-6, 1e-6, 1e-6]]
-        result = matcher.search([query])
+        queries = [[[1, 0.5], [10, 0.5]]]
+        result = matcher.search(queries)
 
-        self.assertAlmostEqual(result[0][0], 1 / 3, 12)
+        self.assertAlmostEqual(result[0][0], 0, 12)
 
 
 if __name__ == "__main__":
