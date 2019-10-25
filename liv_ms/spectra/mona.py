f617faee672b067e49ca9c0eccf0f7179ca1879c
neilswainston <neilswainston@10.24.58.192>
2019-10-25 12:58:46 +0100

    Simplified MoNA data reader.

--- liv_ms/spectra/mona.py
+++ liv_ms/spectra/mona.py
@@ -7,54 +7,51 @@ All rights reserved.
 '''
 import ijson
 import numpy as np
+import pandas as pd
 
 _NAME_MAP = {'kegg': 'kegg.compound',
              'molecular formula': 'formula',
              'total exact mass': 'monoisotopic_mass:float'}
 
 
-def get_spectra(filename, num_spectra=float('inf')):
-    '''Get spectra and metabolite ids.'''
-
-    chemicals = []
-    spectra = []
+def get_spectra(filename, num_spec=float('inf')):
+    '''Get spec and metabolite ids.'''
+    data = []
 
     for prefix, typ, value in ijson.parse(open(filename)):
         if prefix == 'item' and typ == 'start_map':
-            chemical = {}
-            spectrum = {}
+            entry = {}
         elif prefix == 'item.compound.item.inchi':
-            chemical['inchi'] = value
+            entry['inchi'] = value
         elif prefix == 'item.compound.item.names.item.name':
-            if 'name' not in chemical:
-                chemical['name'] = value
+            if 'name' not in entry:
+                entry['name'] = value
             # chemical['names'].append(value)
         elif prefix in ['item.compound.item.metaData.item.name',
                         'item.metaData.item.name']:
             name = _normalise_name(value.lower())
         elif prefix == 'item.compound.item.metaData.item.value':
-            _parse_compound_metadata(name, value, chemical)
+            _parse_compound_metadata(name, value, entry)
             name = None
         elif prefix == 'item.id':
-            spectrum['id'] = value
+            entry['id'] = value
         elif prefix == 'item.metaData.item.value':
-            spectrum[name] = value
+            entry[name] = value
             name = None
         elif prefix == 'item.spectrum':
             values = [float(val) for term in value.split()
                       for val in term.split(':')]
-            spectrum['m/z'] = np.array(values[0::2])
-            spectrum['I'] = np.array(values[1::2])
+            entry['m/z'] = np.array(values[0::2])
+            entry['I'] = np.array(values[1::2])
         # elif prefix == 'item.tags.item.text':
         #    spectrum['tags'].append(value)
         elif prefix == 'item' and typ == 'end_map':
-            chemicals.append(chemical)
-            spectra.append(spectrum)
+            data.append(entry)
 
-            if len(spectra) == num_spectra:
+            if len(data) == num_spec:
                 break
 
-    return chemicals, spectra
+    return pd.DataFrame(data)
 
 
 def _parse_compound_metadata(name, value, chemical):
