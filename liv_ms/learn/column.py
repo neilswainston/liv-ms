1fef1d2c46f62889331d8f551937313d4982abcd
neilswainston <neilswainston@10.24.58.116>
2020-01-06 10:28:19 +0000

    Added hydrophobicity encoding.

--- liv_ms/learn/column.py
+++ liv_ms/learn/column.py
@@ -16,45 +16,53 @@ _DIM_PATTERN = \
 
 _PART_PATTERN = r'(\d+(?:\.\d+)?)(?: )?(?:um|micron|microm)'
 
+_HYDROPHOBIC_PATTERN = r'C\d+|BEH'
+
 
 def encode_column(df):
     '''Encode column.'''
-    dims = df['column'].apply(_get_dims)
-    part_size = df['column'].apply(_get_part_size)
+    df['column_values'] = df.apply(_get_column_values, axis=1)
+
+
+def _get_column_values(row):
+    '''Get column values.'''
+    return _get_dims(row) + [_get_part_size(row), _get_hydrophobic(row)]
 
 
 def _get_dims(row):
     '''Get dimensions.'''
-    mtch = re.search(_DIM_PATTERN, row.lower())
+    mtch = re.search(_DIM_PATTERN, row['column'].lower())
 
     dims = [float('NaN'), float('NaN')]
 
     if mtch:
         dims = sorted(map(float, mtch.groups()))
 
-    print(row, dims)
-
     return dims
 
 
 def _get_part_size(row):
     '''Get particle size.'''
-    mtch = re.search(_PART_PATTERN, row.lower())
+    mtch = re.search(_PART_PATTERN, row['column'].lower())
 
     part_size = float('NaN')
 
     if mtch:
         part_size = float(mtch.group(1))
 
-    print(row, part_size)
-
     return part_size
+
+
+def _get_hydrophobic(row):
+    '''Get hydrophobic.'''
+    return int(bool(re.search(_HYDROPHOBIC_PATTERN, row['column'])))
 
 
 def main(args):
     '''main method.'''
     df = pd.read_csv(args[0])
     encode_column(df)
+    print(df)
 
 
 if __name__ == '__main__':
