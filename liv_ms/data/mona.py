3e2c84bc637213ac34c16e6f2e1b1d6f3a7f9024
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-09-07 13:13:14 +0100

    Added grouping and intensity filtering.

--- liv_ms/data/mona.py
+++ liv_ms/data/mona.py
@@ -6,6 +6,7 @@ All rights reserved.
 @author: neilswainston
 '''
 # pylint: disable=wrong-import-order
+from functools import partial
 import sys
 
 import ijson
@@ -62,7 +63,7 @@ def get_spectra(filename, num_spec=float('inf')):
     return pd.DataFrame(data)
 
 
-def _filter(spectra_df, fltr=None, cols=None):
+def filter_spec(spectra_df, fltr=None, cols=None):
     '''Filter.'''
     fltr_df = spectra_df[cols] if cols else spectra_df
 
@@ -71,6 +72,38 @@ def _filter(spectra_df, fltr=None, cols=None):
             fltr_df = fltr_df[fltr_df[val[0]] == val[1]]
 
     return fltr_df
+
+
+def normalise_inten(spectra_df):
+    '''Normalise intensities.'''
+    spectra_df.loc[:, 'I'] = spectra_df['I'].apply(_normalise_inten)
+
+
+def group(spectra_df, col='smiles'):
+    '''Group.'''
+    grouped = []
+    columns = []
+
+    for _, group_df in spectra_df.groupby([col]):
+        m_z = [val for lst in group_df['m/z'] for val in lst]
+        inten = [val for lst in group_df['I'] for val in lst]
+
+        # Sort by intensity:
+        m_z, inten = list(zip(*sorted(list(zip(m_z, inten)))))
+
+        group_df.drop(['m/z', 'I'], axis=1, inplace=True)
+        grouped.append(group_df.iloc[0, :].tolist() + [m_z, inten])
+        columns = group_df.columns.tolist() + ['m/z', 'I']
+
+    return pd.DataFrame(grouped, columns=columns).set_index('index')
+
+
+def filter_inten(spectra_df, min_inten=0.1):
+    '''Filter by intensity.'''
+    _filter_inten_part = partial(_filter_inten, min_inten=min_inten)
+
+    spectra_df[['m/z', 'I']] = \
+        spectra_df[['m/z', 'I']].apply(_filter_inten_part, axis=1)
 
 
 def _parse_compound_metadata(name, value, chemical):
@@ -89,6 +122,18 @@ def _normalise_name(name):
     return name.replace(':', '_')
 
 
+def _normalise_inten(inten):
+    '''Normalise intensity.'''
+    return inten / inten.max()
+
+
+def _filter_inten(cols, min_inten):
+    '''Filter by intensity.'''
+    return pd.Series([list(val) for val in zip(*[[m, i]
+                                                 for m, i in zip(*cols)
+                                                 if i > min_inten])])
+
+
 def main(args):
     '''main method.'''
     filename = args[0]
@@ -99,15 +144,22 @@ def main(args):
     spec_df['index'] = spec_df.index
 
     # Filter:
-    # fltr_df = _filter(spec_df, [['collision energy', '50 eV']])
-    fltr_df = _filter(spec_df, cols=['index',
-                                     'name',
-                                     'monoisotopic_mass_float',
-                                     'smiles',
-                                     'm/z',
-                                     'I'])
+    spec_df = filter_spec(spec_df, cols=['index',
+                                         'name',
+                                         'monoisotopic_mass_float',
+                                         'smiles',
+                                         'm/z',
+                                         'I'])
 
-    fltr_df.to_csv('spectra.csv', index=False)
+    # Normalise intensities:
+    normalise_inten(spec_df)
+
+    # Group (by SMILES):
+    spec_df = group(spec_df)
+
+    filter_inten(spec_df)
+
+    spec_df.to_csv('spectra.csv', index=False)
 
 
 if __name__ == '__main__':
