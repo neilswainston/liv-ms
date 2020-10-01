5c1d0f98852dcf73babfd459f19c874e5875e360
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-10-01 18:57:46 +0100

    Updated analysis to count and decode bond annotations.

--- liv_ms/learn/analysis.py
+++ liv_ms/learn/analysis.py
@@ -10,22 +10,37 @@ All rights reserved.
 import json
 import sys
 
+from rdkit import Chem
+
 import numpy as np
 import pandas as pd
 
 
-def to_numpy(array_str, sep=','):
+def get_data(filename):
+    '''Get data.'''
+    df = pd.read_csv(filename)
+    df['m/z'] = df['m/z'].apply(_to_numpy, sep=',')
+    df['METFRAG_MZ'] = df['METFRAG_MZ'].apply(_to_numpy, sep=',')
+    df['METFRAG_BROKEN_BONDS'] = df['METFRAG_BROKEN_BONDS'].apply(_to_numpy_2d)
+
+    df['match_idxs'] = df.apply(_match, axis=1)
+    df['bonds_broken'] = df.apply(_get_broken_bonds, axis=1)
+
+    return df
+
+
+def _to_numpy(array_str, sep=','):
     '''Convert array_str to numpy.'''
     return np.fromstring(array_str[1:-1], sep=sep)
 
 
-def to_numpy_2d(array_str):
+def _to_numpy_2d(array_str):
     '''Convert array_str to numpy 2d array.'''
     values = json.loads(array_str)
-    return np.array([np.array(val) for val in values])
+    return np.array([tuple(val) for val in values])
 
 
-def match(row, tol=0.01):
+def _match(row, tol=0.01):
     '''Determine if masses match.'''
     match_idxs = np.ones(row['m/z'].size, dtype=int) * -1
     abs_diff = np.abs(np.subtract.outer(row['m/z'], row['METFRAG_MZ'])) < tol
@@ -35,7 +50,7 @@ def match(row, tol=0.01):
     return match_idxs
 
 
-def get_broken_bonds(row):
+def _get_broken_bonds(row):
     '''Get bonds broken.'''
     broken_bonds = np.empty(row['match_idxs'].size, dtype=object)
     match_idxs = np.where(row['match_idxs'] > -1)
@@ -45,17 +60,43 @@ def get_broken_bonds(row):
     return broken_bonds
 
 
+def _decode(value):
+    '''Decode.'''
+    if isinstance(value, tuple):
+        return tuple([_decode_val(val) for val in value])
+
+    return value
+
+
+def _decode_val(encoded):
+    '''Decode value.'''
+    table = Chem.GetPeriodicTable()
+
+    atomic_number_1 = (encoded & 2**18 - 1) >> 11
+    atomic_number_2 = (encoded & 2**11 - 1) >> 4
+    order_ordinal = (encoded & 2**4 - 1) >> 1
+    aromatic = (encoded & 1) == 1
+
+    return table.GetElementSymbol(atomic_number_1), \
+        table.GetElementSymbol(atomic_number_2), \
+        order_ordinal + 1, \
+        aromatic
+
+
 def main(args):
     '''main method.'''
-    df = pd.read_csv(args[0])
-    df['m/z'] = df['m/z'].apply(to_numpy, sep=',')
-    df['METFRAG_MZ'] = df['METFRAG_MZ'].apply(to_numpy, sep=',')
-    df['METFRAG_BROKEN_BONDS'] = df['METFRAG_BROKEN_BONDS'].apply(to_numpy_2d)
+    df = get_data(args[0])
 
-    df['match_idxs'] = df.apply(match, axis=1)
-    df['bonds_broken'] = df.apply(get_broken_bonds, axis=1)
+    value_counts = pd.Series(
+        [item for sublist in df['bonds_broken']
+         for item in sublist]).value_counts(normalize=True, dropna=False)
 
-    print(df[['match_idxs', 'bonds_broken']])
+    value_counts_df = value_counts.reset_index()
+    value_counts_df['index'] = value_counts_df['index'].apply(_decode)
+    value_counts_df.set_index('index', inplace=True)
+    value_counts_df.index.name = 'bonds'
+    value_counts_df.columns = ['frequency']
+    value_counts_df.to_csv('bonds_frequency.csv')
 
 
 if __name__ == '__main__':
