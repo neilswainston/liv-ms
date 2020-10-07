a50aa9c7045662c6664207f406e9c327f41a92af
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-10-07 11:28:08 +0100

    Calculated individual bond frequencies of matching fragments.

--- liv_ms/learn/analysis.py
+++ liv_ms/learn/analysis.py
@@ -7,6 +7,8 @@ All rights reserved.
 '''
 # pylint: disable=invalid-name
 # pylint: disable=no-member
+import collections
+from functools import partial
 import json
 import sys
 
@@ -16,29 +18,52 @@ import numpy as np
 import pandas as pd
 
 
-def get_bonds_frequency(df):
-    '''Get bonds frequency.'''
-    bonds_frequency = pd.Series(
+def get_bonds_freq(df):
+    '''Get multiple bonds frequency.'''
+    bonds_freq = pd.Series(
         [item for sublist in df['bonds_broken']
          for item in sublist]).value_counts(normalize=True, dropna=False)
 
-    bonds_frequency_df = bonds_frequency.reset_index()
-    bonds_frequency_df['index'] = bonds_frequency_df['index'].apply(_decode)
-    bonds_frequency_df.set_index('index', inplace=True)
-    bonds_frequency_df.index.name = 'bonds'
-    bonds_frequency_df.columns = ['frequency']
+    bonds_freq_df = bonds_freq.reset_index()
+    bonds_freq_df['index'] = bonds_freq_df['index'].apply(_decode)
+    bonds_freq_df.set_index('index', inplace=True)
+    bonds_freq_df.index.name = 'bonds'
+    bonds_freq_df.columns = ['freq']
 
-    return bonds_frequency_df
+    return bonds_freq_df
 
 
-def get_data(filename):
+def get_bond_freq(bonds_freq_df):
+    '''Get individual bond frequency.'''
+    bond_freq = collections.defaultdict(int)
+
+    partial_bond_freq = partial(_get_bond_freq, bond_freq=bond_freq)
+    bonds_freq_df.apply(partial_bond_freq, axis=1)
+
+    data = []
+
+    for key, value in bond_freq.items():
+        data.append(list(key) + [value])
+
+    cols = ['atom1', 'atom2', 'order', 'aromatic', 'match', 'precursor',
+            'freq']
+
+    df = pd.DataFrame(data, columns=cols)
+
+    matched_freq = df.loc[df['match'], 'freq']
+    df.loc[df['match'], 'freq_matched'] = matched_freq / sum(matched_freq)
+
+    return df.sort_values('freq', ascending=False)
+
+
+def get_data(filename, tol):
     '''Get data.'''
     df = pd.read_csv(filename)
     df['m/z'] = df['m/z'].apply(_to_numpy, sep=',')
     df['METFRAG_MZ'] = df['METFRAG_MZ'].apply(_to_numpy, sep=',')
     df['METFRAG_BROKEN_BONDS'] = df['METFRAG_BROKEN_BONDS'].apply(_to_numpy_2d)
 
-    df['match_idxs'] = df.apply(_match, axis=1)
+    df['match_idxs'] = df.apply(partial(_match, tol=tol), axis=1)
     df['bonds_broken'] = df.apply(_get_broken_bonds, axis=1)
 
     return df
@@ -55,7 +80,7 @@ def _to_numpy_2d(array_str):
     return np.array([tuple(val) for val in values])
 
 
-def _match(row, tol=0.01):
+def _match(row, tol):
     '''Determine if masses match.'''
     match_idxs = np.ones(row['m/z'].size, dtype=int) * -1
     abs_diff = np.abs(np.subtract.outer(row['m/z'], row['METFRAG_MZ'])) < tol
@@ -98,11 +123,30 @@ def _decode_val(encoded):
         aromatic
 
 
+def _get_bond_freq(row, bond_freq):
+    '''Get individual bond frequency for given row.'''
+    if isinstance(row.name, float) and np.isnan(row.name):
+        # Special case: unmatched:
+        bond_freq[
+            (None, None, float('NaN'), False, False, False)] += row.freq
+    elif not row.name:
+        # Special case: precursor:
+        bond_freq[
+            (None, None, float('NaN'), False, True, True)] += row.freq
+    else:
+        for bond in row.name:
+            key = tuple(list(bond) + [True, False])
+            bond_freq[key] += row.freq / len(row.name)
+
+
 def main(args):
     '''main method.'''
-    df = get_data(args[0])
-    bonds_frequency_df = get_bonds_frequency(df)
-    bonds_frequency_df.to_csv('bonds_frequency.csv')
+    df = get_data(args[0], float(args[1]))
+    bonds_freq_df = get_bonds_freq(df)
+    bonds_freq_df.to_csv('bonds_freq.csv')
+
+    bond_freq_df = get_bond_freq(bonds_freq_df)
+    bond_freq_df.to_csv('bond_freq.csv', index=False)
 
 
 if __name__ == '__main__':
